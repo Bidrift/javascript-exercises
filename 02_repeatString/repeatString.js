82111752022cd0858fa0bf8a2a16090376353649
Bidrift <37871111+Bidrift@users.noreply.github.com>
2024-08-14 01:25:36 +0100

    Solved 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(ourString, n) {
+    if (n < 0) {
+        return 'ERROR';
+    }
+    let s = "";
+    for (let i = 0; i < n; i++) {
+        s = s.concat(ourString);
+    }
+    return s;
 };
 
 // Do not edit below this line
