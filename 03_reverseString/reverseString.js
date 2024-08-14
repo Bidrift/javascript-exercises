6934a64f60e4f035406c97283553956faca8c2a8
Bidrift <37871111+Bidrift@users.noreply.github.com>
2024-08-14 01:38:45 +0100

    Solve 03, 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(ourString) {
+    const letters = ourString.split("")
+    let resultString = "";
+    for (let i = letters.length - 1; i >= 0; i--) {
+        resultString = resultString.concat(letters[i]);
+    }
+    return resultString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(ourArray, ...Args) {
+    let newArray = [];
+    nextElement: for (const element of ourArray) {
+        for (const arg of Args) {
+            if (arg === element) {
+                continue nextElement;
+            }
+        }
+        newArray.push(element);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
